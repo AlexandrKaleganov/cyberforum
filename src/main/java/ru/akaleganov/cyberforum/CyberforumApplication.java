225ab5d63c55a676a2212a4939eb814b6dcb3945
Alexander Kaleganov <alexmur07@mail.ru>
2020-12-01 01:03:24 +0700

    init

---
+++ src/main/java/ru/akaleganov/cyberforum/CyberforumApplication.java
@@ -0,0 +1,13 @@
+package ru.akaleganov.cyberforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akaleganov/cyberforum/CyberforumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akaleganov.cyberforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
